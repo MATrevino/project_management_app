a834e156f189b7864713ec146b77c971008c2883
Meredith Trevino <meredithtrevino3@gmail.com>
2023-09-23 09:23:15 -0600

    Fixes status error with projects index

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,27 @@
 class Project < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+  def badge_color
+    case self.status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def status
+    return 'not-started' if tasks.none?
+
+    if tasks.all? { |task| task.complete? }
+      'complete'
+    elsif tasks.any? { |task| task.in_progress? || task.complete? }
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
 end
